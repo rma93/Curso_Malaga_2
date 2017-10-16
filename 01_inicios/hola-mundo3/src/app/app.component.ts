dd7a4fc63821a4eae1df8d70efa418fb19894582
rma93 <rubenmena93@hotmail.es>
2017-10-16 16:46:07 +0200

    Hola mundo 3

--- 01_inicios/hola-mundo3/src/app/app.component.ts
+++ 01_inicios/hola-mundo3/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
-  title2 = '2';
+  curso = 'Angular 4';
+  formador = 'Alejandro';
+  empresa = 'Training Consulting';
+  anno = '2017';
 }
